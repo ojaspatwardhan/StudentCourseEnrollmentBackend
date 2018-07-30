e6d4bd3dd6f61311ae5e83be2694fec83fb8aee9
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-07-31 02:40:20 +0530

    url changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 //CORS
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+res.header("Access-Control-Allow-Origin", "https://student-enrollment-angular-app.herokuapp.com");
 res.header("Access-Control-Allow-Headers",
 "Origin, X-Requested-With, Content-Type, Accept");
 res.header("Access-Control-Allow-Methods",
